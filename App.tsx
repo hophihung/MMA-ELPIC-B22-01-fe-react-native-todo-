756adf31d26a2cd6fc2b2f84e44319b7b47adf93
hophihung <148520840+hophihung@users.noreply.github.com>
2025-10-16 23:34:30 +0700

    #29. Sử dụng State và Data Type

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // data-type 
+  // String
+  const [name , setName] = useState<string>('hung');
+  // Number
+  const [age, setAge] = useState<number>(5);
+  // Boolean ,null , undefined
+  const [isStudent, setIsStudent] = useState<boolean>(true);
+  // Array , object , function
+  const [person, setPerson] = useState([{
+    name : 'hung',
+    age : 5,
+  }]);
+
   return (
     <View style={styles.container}>
       <Text style = {styles.hung}>hẹ hẹ e  </Text>
       <Text >hoc di it nua suc mieng   </Text>
+      <StatusBar style="auto" />
       {/* <StatusBar style="auto" /> */}
+
+      <View>
+        <Text style={styles.text}>hello {name} </Text>
+        <Text style={styles.text}>age : {age} </Text>
+        <Text style={styles.text}>isStudent : {isStudent ? 'true' : 'false'} </Text>
+        <Text style={styles.text}>person : {JSON.stringify(person)} </Text>
+
+      </View>
     </View>
 
 
@@ -21,13 +44,22 @@ const styles = StyleSheet.create({
     color : 'red',
     fontSize : 30 , 
   },
+  text : {
+    fontSize : 25,
+    color : 'green',
+    marginTop : 20,
+  },
 
  
 
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    fontSize : 20,color : 'blue',
+    paddingTop :20,
+    paddingHorizontal : 10,
+
   },
 });
